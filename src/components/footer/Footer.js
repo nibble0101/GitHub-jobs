d5eececeec764e718dd90c9cfdcef7438128689f
nibble0101 <mj.mawa@yahoo.com>
2020-08-01 20:33:29 +0300

    Added more styling to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   const date = new Date();
   return (
     <footer className="footer">
-      <p className="date"> Retrieved on: {date.toLocaleString()} </p>
+      <p className="date"> Retrieved on: {date.toDateString()}, {date.toLocaleTimeString()} </p>
       <p className="source">
         Source: <a href="https://jobs.github.com/"> GitHub Jobs </a>
       </p>
